c830728222b8c52e84f218ba0d5187b40b70bb6c
artmas <arte.mas@mail.ru>
2017-09-17 10:52:58 +0500

    test \r\n sniff

--- SecondClass.php
+++ SecondClass.php
@@ -9,6 +9,8 @@ class SecondClass
     public function go() {
         $line = "this is line \r\n And new line god damm";
         $a = 'hello! \r\n !!!';
+        $b = "big text
+            and new line";
     }
 
 }
